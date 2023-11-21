f9a398db72b22d9b4bd87e7a5caae9b594a20ec4
Anushakrupakar <101596725+Anushakrupakar@users.noreply.github.com>
2023-11-21 07:01:18 +0530

    Update sample_code.sh

--- sample_code.sh
+++ sample_code.sh
@@ -3,7 +3,5 @@ echo "This is an Samples Script from GitHub"
 echo "This is test from Linux Server"
 echo "This is an example of a script"
 echo "This is running in a build step and it shows changes"
-echo "new commit"
-echo "new commit1"
 SUM=$((FIRSTNUMBER + SECONDNUMBER))
 echo "$SUM"
